be3ed73b29a86759aba09d1247fbdc085a5917b3
MC Thanh Ngoc <thanhngocmc1990@gmail.com>
2024-09-23 22:08:26 +0700

    first commit - done

---
+++ Bai01_Hello/src/MyClass.java
@@ -0,0 +1,17 @@
+public class MyClass {
+
+	/**
+	 * @access public
+	 * @param array
+	 * @return null
+	 */
+	public static void main(String[] args) {
+		// Khai báo biến
+		
+		int a = 2, b=5;
+		int c = a+b;
+		System.out.println("Everybody has to start from Hello World :)");
+		System.out.println("Tổng c là: "+c);
+	}
+
+}
